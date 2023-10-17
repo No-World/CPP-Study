ad4f3e47ce087912c5e8495b132ec81e1877364a
NoWorld <2259881867@qq.com>
2023-10-17 10:51:42 +0800

    new file:   AcWing/4736.cpp 	new file:   AcWing/4736.exe 	modified:   muban.cpp 	modified:   muban.exe

--- AcWing/4736.cpp
+++ AcWing/4736.cpp
@@ -1,3 +1,11 @@
+// Problem: 步行者
+// Contest: AcWing
+// URL: https://www.acwing.com/problem/content/4739/
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
 /*
 ⣿⣿⣿⣿⣿⣿⡷⣯⢿⣿⣷⣻⢯⣿⡽⣻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⠸⣿⣿⣆⠹⣿⣿⢾⣟⣯⣿⣿⣿⣿⣿⣿⣽⣻⣿⣿⣿⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣻⣽⡿⣿⣎⠙⣿⣞⣷⡌⢻⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⡄⠹⣿⣿⡆⠻⣿⣟⣯⡿⣽⡿⣿⣿⣿⣿⣽⡷⣯⣿⣿⣿⣿⣿⣿
@@ -58,7 +66,7 @@
 #include <cstdio>
 // #include <iostream>
 // #include <cstring>
-// #include <algorithm>
+#include <algorithm>
 // #include <cmath>
 // #include <queue>
 // #include <map>
@@ -69,11 +77,30 @@
 // #include <cstdlib>
 // typedef long long ll;
 using namespace std;
-const int inf = 0x3f3f3f3f, N = 1e3 + 10, mod = 1e9 + 7;
-// const ll INF = __LONG_LONG_MAX__, MOD;
+const int inf = 0x3f3f3f3f, N = 1e3 + 10;
+// const ll INF = __LONG_LONG_MAX__;
 
-inline void Solution(int T)
+int race[31][N];
+
+inline void Solution(int t)
 {
+    int n, m, p, ans = 0, pr;
+    scanf("%d%d%d", &m, &n, &p);
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &race[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int pr = race[i][p - 1];
+        sort(race[i], race[i] + m);
+        ans += race[i][m - 1] - pr;
+    }
+
+    printf("Case #%d: %d\n", t + 1, ans);
 }
 
 int main(int argc, char const *argv[])
@@ -83,7 +110,7 @@ int main(int argc, char const *argv[])
     // std::cin.tie(nullptr);
     // std::cout.tie(nullptr);
     // cin >> T;
-    // scanf("%d", &T);
+    scanf("%d", &T);
     for (int i = 0; i < T; i++)
     {
         Solution(i);
