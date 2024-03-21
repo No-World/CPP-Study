6b42143cdb858ed249eaf25a3abcfb3c2d67f73d
NoWorld <2259881867@qq.com>
2024-03-21 18:55:57 +0800

    modified:   .vscode/c_cpp_properties.json 	modified:   .vscode/launch.json 	modified:   .vscode/tasks.json 	modified:   1.c 	modified:   1.exe 	modified:   acm/03/test3-1-1.exe 	modified:   old/01.cpp 	modified:   old/01.exe

--- old/01.cpp
+++ old/01.cpp
@@ -1,14 +1,6 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+#include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    int a[1000] = {0, 1};
-    for (int i = 0; i < 1000; i++)
-    {
-        printf("%d\n", a[i]);
-    }
-
-    return 0;
+    printf("0");
 }
