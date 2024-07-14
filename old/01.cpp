4f01985738cf7fa52012b95a03e9a7433d758152
No_World <2259881867@qq.com>
2024-07-14 16:44:04 +0800

    modified:   old/01.cpp 	modified:   old/01.exe 	modified:   old/02.cpp

---
+++ old/01.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#define MaxSize 1010
+typedef int ElemType;
+struct Sqlist
+{
+    ElemType data[MaxSize];
+    int size;
+};
+
+int BinarySearch(Sqlist *List, ElemType x)
+{
+    int l = 0, r = List->size - 1;
+    while (l < r)
+    {
+        int mid = (l + r + 1) >> 1;
+        if (List->data[mid] > x)
+        {
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid;
+        }
+    }
+    if (List->data[r] == x)
+        return r;
+    return -1;
+}
+
+int main()
+{
+    Sqlist a;
+    a.size = 0;
+    for (int i = 0; i <= 9999; ++i)
+    {
+        a.data[i] = i;
+        a.size++;
+    }
+    for (int i = 0; i <= 9999; ++i)
+    {
+        if (i != BinarySearch(&a, i))
+            std::cout << i << ":no" << '\n';
+    }
+    return 0;
+}
---
+++ old/02.cpp
@@ -0,0 +1,7 @@
+#include <algorithm>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    sort();
+    return 0;
+}
